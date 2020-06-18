e903813e3b4f65e1d8e61dea623b01c0e1eb3a76
Travis Shears <btbtravis@gmail.com>
2020-06-18 08:06:27 +0200

    fix local db auth

--- app/__init__.py
+++ app/__init__.py
@@ -5,23 +5,21 @@ from flask_cors import CORS
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__, static_folder='react_app')
+CORS(app)
 
-# mongo setup
+# init mongo
 mongo_user = os.environ['MONGO_USER']
 mongo_pw = os.environ['MONGO_PW']
+mongo_db = os.environ['MONGO_DB']
 mongo_host = os.environ.get('MONGO_HOST', 'localhost')
 app.config['MONGODB_SETTINGS'] = {
     'username': mongo_user,
     'password': mongo_pw,
     'host': mongo_host,
-    'db': "rediRun"
+    'db': mongo_db
 }
 db = MongoEngine()
 db.init_app(app)
-
-#cors setup
-CORS(app)
-
 
 app_secret_key = os.environ['APP_SECRET_KEY']
 app.secret_key = bytes(app_secret_key, 'utf-8')
